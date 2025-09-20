83d6a87416c04a69894479b8e35d8305be068297
Романов Богдан <137334311+BogdanRomanovvv@users.noreply.github.com>
2025-09-20 12:43:57 +0300

    Remove build directory from repository, now ignored by .gitignore

--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ChocolateDivider/tests
-# Build directory: D:/ChocolateDivider/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runTests "D:/ChocolateDivider/build/tests/runTests.exe")
-set_tests_properties(runTests PROPERTIES  _BACKTRACE_TRIPLES "D:/ChocolateDivider/tests/CMakeLists.txt;20;add_test;D:/ChocolateDivider/tests/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
